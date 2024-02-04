097db4b98f2c7e60056f8e885ab58619aa8f0ce8
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-02-04 20:59:42 +0600

    homebanar_uptade_0003

--- src/Pages/Home/HomeSection/Homebanar/Homebanar.jsx
+++ src/Pages/Home/HomeSection/Homebanar/Homebanar.jsx
@@ -11,54 +11,65 @@ import sassicon from '../../../../images/sassicon.png'
 import reacticon from '../../../../images/reacticon.png'
 import tailwindicon from '../../../../images/tailwindicon.png'
 import image from '../../../../images/3dimage.jpg'
+import { CiLinkedin } from "react-icons/ci";
+import { FiGithub } from "react-icons/fi";
+import { Link } from 'react-router-dom'
+import Nanohead from '../../../../Utilities/Nanohead/Nanohead'
 
 const Homebanar = () => {
   return (
-    <section id='homebanar'>
+      <section id='homebanar'>
         <div className='container'>
+          <div className='homebanar_wrapper_full_contant'>
             <div className='homebanar_wrapper'>
-                <div className='homebanar_contant_box'>
-                    <Head text="Full Stack Developer" style="homehaner_heading"/>
-                    <Pera text="Hi,I'm Yeasin Munshi. A passionate Full Stack Developer based in Bangladesh." style="homebanar_pera"/>
+              <div className='homebanar_contant_box'>
+                <Head text="Full Stack Developer" style="homehaner_heading"/>
+                <Pera text="Hi,I'm Yeasin Munshi. A passionate Full Stack Developer based in Bangladesh." style="homebanar_pera"/>
+                <div className='homebanar_wrapper_icon_box'>
+                  <Link to = "https://www.linkedin.com/feed/" target='blank'><CiLinkedin className='homebanar_wrapper_icon' /></Link>
+                  <Link to = "https://github.com/" target='blank'><FiGithub className='homebanar_wrapper_icon' /></Link>
                 </div>
-                <div className='homebanar_image_box'>
-                    <div className='homebanar_image_box_flex'>
-                      <div className='homebanar_small_image_box_flex'>
-                        <div className='homebanar_small_image_box'>
-                          <img src={htmlicon} alt="nto found" />
-                        </div>
-                        <div className='homebanar_small_image_box'>
-                          <img src={cssicon} alt="nto found" />
-                        </div>
-                        <div className='homebanar_small_image_box'>
-                          <img src={reacticon} alt="nto found" />
-                        </div>
-                        <div className='homebanar_small_image_box'>
-                          <img src={sassicon} alt="nto found" />
-                        </div>
-                        <div className='homebanar_small_image_box'>
-                          <img src={tailwindicon} alt="nto found" />
-                        </div>
-                      </div>
-                      <div className='homebanar_big_image_box'>
-                          <img src={image} alt="not found" />
-                      </div>
-                    </div>
-                    <div className='homebanar_small_image_box_two_flex'>
-                        <div className='homebanar_small_image_box_two'>
-                          <img src={javascripticon} alt="nto found" />
-                        </div>
-                        <div className='homebanar_small_image_box_two'>
-                          <img src={nodejsicon} alt="nto found" />
-                        </div>
-                        <div className='homebanar_small_image_box_two'>
-                          <img src={mongodbicon} alt="nto found" />
-                        </div>
-                    </div>
+              </div>
+              <div className='homebanar_image_box'>
+                <div className='homebanar_big_image_box'>
+                  <img src={image} alt="not found" />
                 </div>
+              </div>
             </div>
+            <div className='homebanar_all_language_wrapper'>
+              <div className='homebanar_all_language_wrapper_stack'>
+                <Nanohead text="Tech Stack" style="homebanar_all_language_wrapper_stack_name"/>
+              </div>
+              <div className='homebanar_all_language_wrapper_flex'>
+                <div className='homebanar_small_image_box'>
+                  <img src={htmlicon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box'>
+                  <img src={cssicon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box'>
+                  <img src={reacticon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box'>
+                  <img src={sassicon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box'>
+                  <img src={tailwindicon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box_two'>
+                  <img src={javascripticon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box_two'>
+                  <img src={nodejsicon} alt="nto found" />
+                </div>
+                <div className='homebanar_small_image_box_two'>
+                  <img src={mongodbicon} alt="nto found" />
+                </div>    
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
+      </section>
   )
 }
 
