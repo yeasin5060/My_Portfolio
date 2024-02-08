e8401ad7204dbd1da7332e85ed0b58d330ae6d76
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-02-08 16:35:15 +0600

    update_project_section_002

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Projectbanar from './Projectsection/Projectbanar/Projectbanar'
+import Projectnew from './Projectsection/Projectnew/Projectnew'
 
 const Project = () => {
   return (
    <>
       <Projectbanar/>
+      <Projectnew/>
    </>
   )
 }
