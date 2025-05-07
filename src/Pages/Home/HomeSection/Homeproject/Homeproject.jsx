3d0c9a56d3568638e5e834b594d1e4bbd9267995
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2025-05-07 11:22:03 +0600

    update home project section

--- src/Pages/Home/HomeSection/Homeproject/Homeproject.jsx
+++ src/Pages/Home/HomeSection/Homeproject/Homeproject.jsx
@@ -9,11 +9,13 @@ import reatestate from '../../../../images/realestate.png'
 import management from '../../../../images/management.png'
 import clinic from '../../../../images/clinic.png'
 import chat from '../../../../images/chat.png'
+import metablog from '../../../../images/metablog.png'
+import funiro from '../../../../images/funirobanner.png'
 
 let arraypro = [
     {
         id : 1,
-        projectname : "Ecommerce",
+        projectname : "Amazon front clone",
         img :ecommerce,
         pera : "An ecommerce website is your digital storefront on the internet. It facilitates the transaction between a buyer and seller. It is the virtual space where you showcase products, and online customers make selections. Your website acts as the product shelves, sales staff, and cash register of your online business channel.",
         btn : "Live Link",
@@ -50,6 +52,22 @@ let arraypro = [
         pera : "Welcome to Clinic , your trusted healthcare provider offering a wide range of medical services to meet your health needs. Our clinic is dedicated to providing high-quality, compassionate care in a comfortable and modern environment.",
         btn : "Live Link",
         link : "https://keen-pika-e61b86.netlify.app"
+    },
+    {
+        id : 6,
+        projectname : "MetaBlog",
+        img :metablog,
+        pera : "MetaBlog is a modern and responsive blogging website built using React.js. It allows users to explore blog posts, read articles, and view content categorized by topics. The design is clean, minimal, and optimized for readability. MetaBlog is ideal for developers, writers, and content creators looking to publish and showcase articles in a stylish format.",
+        btn : "Live Link",
+        link : "https://stalwart-salmiakki-fb846c.netlify.app/"
+    },
+    {
+        id : 7,
+        projectname : "Funiro eCommerce",
+        img :funiro,
+        pera : "Furniro is a modern, fully functional full-stack e-commerce application designed for a furniture store. It allows users to browse, search, and purchase products while providing an admin dashboard to manage the store efficiently. Built using the MERN stack (MongoDB, Express.js, React, Node.js), this project demonstrates end-to-end development skills including front-end UI/UX, RESTful APIs, and backend integration",
+        btn : "Live Link",
+        link : "https://funiroecommerce.netlify.app/"
     },
 ]
 
